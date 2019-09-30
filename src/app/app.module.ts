686940ff5083bda5fb729e2ff697c6a2b209ec5f
Janardan <jchaugule1990@gmail.com>
2019-10-01 00:24:19 +0530

    first push activity on master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Import RouterModule
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+// Each route maps a URL path to a component
+// The 3rd route specifies the route to redirect to if the path
+// is empty. In our case we are redirecting to /list
+
+// pathMatch property value can be full or prefix. For now we
+// will set it to full as we want to do a full match. In our upcoming videos,
+// we will discuss the difference between prefix and full in detail.
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  saveEmployee(employeeForm: NgForm): void {
+    console.log(employeeForm);
+  }
+
+}
